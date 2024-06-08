1d313fc7dd9fe0e97de4d6f1c5ea65eb3523879b
tanzid64 <tanzid@inbox.ru>
2024-06-09 02:26:50 +0600

    Redux Persist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import { baseApiSlice } from "./apiSlices/baseApiSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+//* Combining all reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+  [baseApiSlice.reducerPath]: baseApiSlice.reducer,
+});
+
+//* Redux Persist Configuration
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    [baseApiSlice.reducerPath]: baseApiSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(baseApiSlice.middleware),
 });
+
+export const persistor = persistStore(store);
